1fd756794b71c19d9bcfd12364da1992958a2c66
tkyzp <1543060148@qq.com>
2020-03-18 22:08:27 +0800

    完成重定位，修改了内存权限设置的位置

--- LoadLibraryP/loadlibraryp.cpp
+++ LoadLibraryP/loadlibraryp.cpp
@@ -98,6 +98,7 @@ int main(int argc, char* argv[]) {
 		over.Offset = section_header[i].PointerToRawData;
 		ReadFile(fhandle, buffer, section_header[i].SizeOfRawData, &readsize, &over);
 		//复制节
+		/*
 		//获取节权限
 		switch (section_header[i].Characteristics & 0xa0000000) {
 		case IMAGE_SCN_MEM_EXECUTE:
@@ -112,6 +113,7 @@ int main(int argc, char* argv[]) {
 		default:
 			access = PAGE_READONLY;
 		}
+		*/
 		//计算虚拟地址
 		VA = (DWORD)image_base + section_header[i].VirtualAddress;
 		//复制内容到虚拟地址
@@ -120,13 +122,30 @@ int main(int argc, char* argv[]) {
 		dest = VirtualAlloc((void*)VA,
 			section_header[i].SizeOfRawData,
 			MEM_COMMIT,
-			access);
+			PAGE_READWRITE);
 		if (dest == NULL) return 4;
 		free(buffer);
 	}
-
-	if (!RElocation) {
-
+	//重定位
+	if (RElocation) {
+		//读取重定位表
+		DWORD reloc_va = (DWORD)image_base + ntheader.OptionalHeader.DataDirectory[5].VirtualAddress;//重定位表虚拟地址
+		DWORD reloc_size = ntheader.OptionalHeader.DataDirectory[5].Size;//重定位表大小
+		DWORD reloc_offset = 0;//重定位块在重定位节中偏移
+		IMAGE_BASE_RELOCATION cur_reloc_tab = { 0 };
+		WORD* TypeOffset = NULL;
+		while (reloc_offset < reloc_size)
+		{
+			cur_reloc_tab = *(IMAGE_BASE_RELOCATION*)(reloc_va + reloc_offset);//当前重定位块头
+			TypeOffset = (WORD*)(reloc_va + reloc_offset + 0x8);//重定位项数组
+			//需要修改重定位处
+			for (int i = 0; i < cur_reloc_tab.SizeOfBlock / 2 - 4; i++) {
+				if ((TypeOffset[i] & 0xf000) == 0x3000) {
+					*(DWORD*)((DWORD)image_base + cur_reloc_tab.VirtualAddress + (TypeOffset[i] & 0xfff)) += (DWORD)image_base - ntheader.OptionalHeader.ImageBase;
+				}
+			}
+			reloc_offset += cur_reloc_tab.SizeOfBlock;
+		}
 	}
 
 
@@ -140,6 +159,29 @@ int main(int argc, char* argv[]) {
 	//解决导出表（刘进）
 	//TODO:
 
+	//内存保护，设置内存权限
+	for (int i = 0; i < ntheader.FileHeader.NumberOfSections; i++) {
+		//获取节权限
+		switch (section_header[i].Characteristics & 0xa0000000) {
+		case IMAGE_SCN_MEM_EXECUTE:
+			access = PAGE_EXECUTE_READ;
+			break;
+		case IMAGE_SCN_MEM_WRITE:
+			access = PAGE_READWRITE;
+			break;
+		case IMAGE_SCN_MEM_EXECUTE | IMAGE_SCN_MEM_WRITE:
+			access = PAGE_EXECUTE_READWRITE;
+			break;
+		default:
+			access = PAGE_READONLY;
+		}
+		//计算虚拟地址
+		VA = (DWORD)image_base + section_header[i].VirtualAddress;
+		dest = VirtualAlloc((void*)VA,
+			section_header[i].SizeOfRawData,
+			MEM_COMMIT,
+			access);
+	}
 	//调用dll入口
 
 
